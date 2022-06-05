7fe027a930b4108a32a39387195d8c3c40a84a27
Lucia Gil <luciagil5952@gmail.com>
2022-06-05 14:52:14 -0400

    Added tip calculator logic

---
+++ script.js
@@ -0,0 +1,23 @@
+var tipPercent = $("#tipPercent");
+var total = $("#total");
+var submitBtn = $("#submit");
+
+// When button
+submitBtn.on('click', function(){
+    // Gather the value for the total bill
+    var billVal = total.val();
+    // Gather the value for the tip percentage
+    var tipPercentVal =  tipPercent.val();
+     // Calculates and stores the value of the tip amount
+    var tipAmountVal = billVal*(tipPercentVal/100);
+    // Calculates and stores the total bill amount using parseFloat to add the values together
+    var totalBillVal = parseFloat(billVal) + parseFloat(tipAmountVal);
+
+     // Displays tip amount on the screen as a number with two decimals
+    var tipamount = $("#tip-amount").text((tipAmountVal.toFixed(2)));
+
+    // Displays total bill amount on the screen as a number with two decimals
+    var totalBill = $("#totalAmount").text(totalBillVal.toFixed(2));
+
+
+})
