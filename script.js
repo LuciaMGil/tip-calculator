b4d85e2cf5bfac199bf7b98aa7ed93d1bf744c99
Lucia Gil <luciagil5952@gmail.com>
2022-06-05 15:57:41 -0400

    Added styling with bootstrap and added ability to split bill

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 var tipPercent = $("#tipPercent");
 var total = $("#total");
 var submitBtn = $("#submit");
+var splitTotal = $("#splitTotal");
 
 // When button
 submitBtn.on('click', function(){
-    // Gather the value for the total bill
+    // Changes button color on click
+    $(this).addClass("buttonColor");
+    // Gather the value for the total bill 
     var billVal = total.val();
     // Gather the value for the tip percentage
     var tipPercentVal =  tipPercent.val();
-     // Calculates and stores the value of the tip amount
+    // Gather the value for the number of people, if no input, set  value to 1
+    var splitNum = splitTotal.val() || 1;
+
+    // Calculates and stores the value of the tip amount
     var tipAmountVal = billVal*(tipPercentVal/100);
+
     // Calculates and stores the total bill amount using parseFloat to add the values together
-    var totalBillVal = parseFloat(billVal) + parseFloat(tipAmountVal);
+    var totalBillVal = (parseFloat(billVal) + parseFloat(tipAmountVal))/splitNum;
 
      // Displays tip amount on the screen as a number with two decimals
-    var tipamount = $("#tip-amount").text((tipAmountVal.toFixed(2)));
+    var tipamount = $("#tip-amount").text("$" + (tipAmountVal.toFixed(2)));
 
     // Displays total bill amount on the screen as a number with two decimals
-    var totalBill = $("#totalAmount").text(totalBillVal.toFixed(2));
-
-
+    var totalBill = $("#totalAmount").text("$" +totalBillVal.toFixed(2));
+    
 })
